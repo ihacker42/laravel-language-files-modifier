855b2d58bea595f151f1196ab67e760b4d00e36c
Hacker <hiteshbansal42@gmail.com>
2021-11-25 17:08:26 +0530

    Minor Fixes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 $locale = Request::segment(1);
 if (in_array($locale, Config::get("app.locales"))) {
     \App::setLocale($locale);
